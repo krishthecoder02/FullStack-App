d0e5f1de307f663368c49f48128520deae270c28
KRISHNA KUMAR MANDAL <118360906+coderkrishnabhai@users.noreply.github.com>
2024-07-29 22:42:02 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
+import Login from "./screen/auth/Login";
+import Register from "./screen/auth/Register";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Register from './screen/auth/Register';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Register/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
 
